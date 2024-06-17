db9509cf36f620c97a164e3e208e44a28cca70a6
JiScPe <jetsada.pra@hotmail.com>
2024-06-17 13:56:36 +0700

    add plant selection in navbar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,5 @@
+import { redirect } from "next/navigation";
 import MainNavigation from "./components/MainNavigation";
-import MyThemeProvider from "./components/MyThemeProvider";
 import "./globals.css";
 import { Onest } from "next/font/google";
 
@@ -10,29 +10,17 @@ export const metadata = {
 
 const onest = Onest({ subsets: ["latin"], display: "swap" });
 
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={onest.className} suppressHydrationWarning={true}>
-      <head>
-        {/* <script
-          dangerouslySetInnerHTML={{
-            __html: `
-          if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-            document.documentElement.classList.add('dark')
-          } else {
-            document.documentElement.classList.remove('dark')
-          }
-          `,
-          }}
-        /> */}
-      </head>
       <body className="flex flex-col bg-slate-200 dark:bg-opacity-0 min-h-[100dvh]">
-        <MyThemeProvider attribute="class">
+        <header>
           <MainNavigation />
-          <main className="flex-grow overflow-y-auto container-sm mx-0">
-            {children}
-          </main>
-        </MyThemeProvider>
+        </header>
+        <main className="flex-grow overflow-y-auto container-sm mx-0">
+          {children}
+        </main>
       </body>
     </html>
   );
